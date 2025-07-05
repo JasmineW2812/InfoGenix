33dd5b4ffda047c63061eed17be03bc4c5a64c66
JasmineW2812 <c3021544@hallam.shu.ac.uk>
2025-07-05 14:16:03 +0100

    adding tailwind

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
+import "../styles/Form.css"
+
 function NotFound() {
     return <div>
         <h1>404 Not Found</h1>
         <p>The page you're looking for doesn't exist!</p>
+
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     </div>
 }
 
